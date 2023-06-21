3c214d2beec14f255e1d0753273a2514f3afa539
Nellenan <nikitasergeev06310@gmail.com>
2023-06-21 15:47:28 +0300

    Вторая попытка

--- module 4/lesson1/hw.py
+++ module 4/lesson1/hw.py
@@ -1,3 +1,6 @@
-s = input()
-h = len(s) // 2
-print(s[:h] == s[:len(s)-h-1:-1])
+def palindrome(s):
+    h = len(s) // 2
+    print(s[:h] == s[:len(s) - h - 1:-1])
+
+
+palindrome('лессел')  # Ввод слова для проверки на палиндром
